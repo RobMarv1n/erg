8e241b96b99331369ba6e63e1d48a3707e09401b
RobMarv1n <88106847+RobMarv1n@users.noreply.github.com>
2021-08-10 23:01:59 +0300

    Add files via upload

---
+++ assets/jquery/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-container', {
+
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  autoplay: 4000,
+  speed: 400,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+});
+
